66c1bf05dd5bab4846c7678ce26a62f53501b25b
azuki774 <azuki774s@gmail.com>
2023-03-19 16:44:18 +0900

    recent date genenrate dummy

--- src/flaskapp.py
+++ src/flaskapp.py
@@ -9,12 +9,19 @@ app = Flask(__name__)
 def index():
     # GET FROM MAWINTER-API
     mes = api.getBase()
-    alphabet = ["A", "B", "C", "D", "E"]
-    no = ["One", "Two", "Three", "Four", "Five"]
+
+    # IT IS DUMMY
+    connectionMessage = "ver hoge"
+    id = [5, 4, 3, 2, 1]
+    cat_name = ["食費", "食費", "食費", "食費", "食費"]
+    price = [1000, 2000, 3000, 4000, 5000]
+    date = ["2023-03-01", "2023-03-01", "2023-03-01", "2023-03-01", "2023-03-01"]
+    cat_opts = ["200 - 食費", "201 - 生活用品"]
 
     return render_template(
         "index.html",
-        connectionMessage="ここにAPIサーバとの接続状態が表示されます",
+        connectionMessage=connectionMessage,
         resultMessage="ここに送信結果が出ることになっています",
-        data=zip(alphabet, no),
+        recent_data=zip(id, cat_name, price, date),
+        cat_opts=cat_opts,
     )
