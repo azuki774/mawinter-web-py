a181019e6a5295e74d0ca64dee2db9c1f36f321f
azuki774 <azuki774s@gmail.com>
2023-03-19 15:37:49 +0900

    dynamic generate test

--- src/flaskapp.py
+++ src/flaskapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import api
+import generate
 
 app = Flask(__name__)
 
@@ -8,8 +9,12 @@ app = Flask(__name__)
 def index():
     # GET FROM MAWINTER-API
     mes = api.getBase()
+    alphabet = ["A", "B", "C", "D", "E"]
+    no = ["One", "Two", "Three", "Four", "Five"]
+
     return render_template(
         "index.html",
         connectionMessage="ここにAPIサーバとの接続状態が表示されます",
         resultMessage="ここに送信結果が出ることになっています",
+        data=zip(alphabet, no),
     )
