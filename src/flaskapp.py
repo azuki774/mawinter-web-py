729a24652b77d906fa106aff2da847b7de45262d
azuki774 <azuki774s@gmail.com>
2023-03-19 13:24:42 +0900

    add form table wip

--- src/flaskapp.py
+++ src/flaskapp.py
@@ -6,11 +6,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", message="not sent")
+    return render_template(
+        "index.html",
+        connectionMessage="ここにAPIサーバとの接続状態が表示されます",
+        resultMessage="ここに送信結果が出る予定です",
+    )
 
 
 @app.route("/post", methods=["POST"])
 def post():
     # GET FROM MAWINTER-API
     mes = api.getBase()
-    return render_template("index.html", message=mes)
+    return render_template(
+        "index.html",
+        connectionMessage="ここにAPIサーバとの接続状態が表示されます",
+        resultMessage="ここに送信結果が出ることになっています",
+    )
