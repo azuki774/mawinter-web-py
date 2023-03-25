af026c60f3e4a8616af1f52ca1c11284cd89bb17
azuki774 <azuki774s@gmail.com>
2023-03-25 20:44:24 +0900

    post record

--- src/flaskapp.py
+++ src/flaskapp.py
@@ -7,17 +7,41 @@ app = Flask(__name__)
 ver_info_text = api.getVersion()
 
 
-@app.route("/")
-def index():
-    id, cat_name, price, date = api.getRecent()
-
+@app.route("/", methods=["GET"])
+def index_get():
     cat_opts = ["200 - 食費", "201 - 生活用品"]
-
-    recent_data = api.getRecent()
+    resultMessage = "入力してください"
+    ids, cat_names, prices, dates = api.getRecent()
     return render_template(
         "index.html",
         connectionMessage=ver_info_text,
-        resultMessage="ここに送信結果が出ることになっています",
-        recent_data=zip(id, cat_name, price, date),
+        resultMessage=resultMessage,
+        recent_data=zip(ids, cat_names, prices, dates),
         cat_opts=cat_opts,
     )
+
+
+@app.route("/post", methods=["POST"])
+def index_post():
+    # post 処理
+    post_category_id, post_price = _extract_post_data()
+    if post_price != 0:
+        ret = api.post_record(post_category_id, post_price)
+
+    return render_template(
+        "post.html",
+    )
+
+
+def _extract_post_data():
+    post_category = request.form.getlist("category_selector")
+
+    try:
+        post_category_id = int(post_category[0][:3])
+    except Exception as e:
+        print(e)
+        return "", 0
+
+    post_price = request.form.get("pricebox")
+    print("post_category_id = {}, post_price = {}".format(post_category_id, post_price))
+    return post_category_id, post_price
