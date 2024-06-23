283bd658713ddf934670b6571191d63b7e9c9f86
azuki774s <azuki774s@gmail.com>
2024-06-23 16:31:17 +0900

    fixup! fix: index out range

--- src/flaskapp.py
+++ src/flaskapp.py
@@ -82,8 +82,7 @@ def index_post():
         return redirect(url_for("index.html"))
 
     # post 処理
-    print(request.form.getlist("category_selector"))
-    post_category = request.form.getlist("category_selector")
+    post_category = request.form.get('category_selector')
     post_category_id, post_price = _extract_post_data(post_category)
     if post_price != 0:
         ret = api.post_record(post_category_id, post_price)
@@ -128,7 +127,7 @@ def summary_get():
 # _extract_post_data は request をもとに、record を POSTするための整形処理
 def _extract_post_data(post_category):
     try:
-        post_category_id = int(post_category[0][:3])
+        post_category_id = int(post_category[:3])
         post_price = int(request.form.get("pricebox"))
     except Exception as e:
         logger.error(e)
